95b0f9d057e0931a4b269e9d0d8c48567ecb697f
Reyhaneh <reihane.eng@gmail.com>
2024-11-19 21:00:04 +0330

    one

---
+++ login/welcom.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, this is my first React Native app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#add8e6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ff6347', 
+    fontSize: 19,    
+    fontWeight: 'bold', 
+  },
+});
